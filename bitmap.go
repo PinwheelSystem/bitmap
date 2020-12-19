1640c75c3c02aefdbd2fb4be02a6346d728d13f1
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-12-19 16:55:13 -0400

    add comments and remove logging

--- bitmap.go
+++ bitmap.go
@@ -1,3 +1,37 @@
+/* 
+The bitmap package loads a bitmap font from an image using a descriptor file.
+
+A bitmap font is a font with glyphs that is literally bitmapped, with a transparent pixel being `0`,
+and any opaque pixel being `1`.
+
+A descriptor file describes the size (width, height) of each glyph. It is a JSON file, the same name as the font itself.
+An example: the font is `fnt.png`, the descriptor will be `fnt.json`, and would look like:
+`
+[
+	{
+		"Char": " ",
+		"Width": 7,
+		"X": 1,
+		"Y": 8,
+		"W": 2,
+		"H": 0,
+		"Ox": 0,
+		"Oy": 0
+	},
+	{
+		"Char": "!",
+		"Width": 5,
+		"X": 4,
+		"Y": 1,
+		"W": 3,
+		"H": 7,
+		"Ox": 0,
+		"Oy": 7
+	},
+
+	...
+`
+*/
 package bitmap
 
 import (
@@ -11,25 +45,31 @@ import (
 	_ "image/png"
 )
 
+// BitmapFont holds a slice of Glyphs.
 type BitmapFont struct {
 	font map[string]Glyph
 }
 
+// GlyphInfo is the info about a glyph obtained from the descriptor file.
 type GlyphInfo struct {
 	Char string
 	Width int
 	X, Y, W, H, Ox, Oy int
 }
 
+// Glyph is a single character from the font.
 type Glyph struct {
 	Data []string
 	Width, Height, Y int
 }
 
+// Returns a new, empty bitmap font.
 func New() BitmapFont {
 	return BitmapFont{font: make(map[string]Glyph, 94)	}
 }
 
+// Loads a bitmap font from `filename`.
+// `filename` must include the extension.
 func (b *BitmapFont) Load(filename string) map[string]Glyph {
 	file, _ := os.Open(filename)
 	image, _, _ := image.Decode(file)
@@ -47,7 +87,6 @@ func (b *BitmapFont) Load(filename string) map[string]Glyph {
 		xoffset := ch.X
 		glyph := make([]string, ch.H + 1)
 		binrep := ""
-		fmt.Println(c)
 
 		for y := 0; y < ch.H + 1; y++ {
 			for x := 0; x < ch.W; x++ {
@@ -60,7 +99,6 @@ func (b *BitmapFont) Load(filename string) map[string]Glyph {
 				}
 			}
 			glyph[y] = binrep
-			fmt.Println(binrep)
 			binrep = ""
 		}
 		b.font[c] = Glyph{glyph, ch.W, ch.H + 1, ch.Y}
