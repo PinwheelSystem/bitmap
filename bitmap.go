faddecacf24d351c80e816da1933bd999c3cfcec
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-12-18 20:26:13 -0400

    use descriptor file

--- bitmap.go
+++ bitmap.go
@@ -4,12 +4,21 @@ import (
 	"os"
 	"image"
 	"strings"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
 
 	_ "image/png"
 )
 
 type BitmapFont struct {
 	font map[string][]string
+}
+
+type GlyphInfo struct {
+	Char string
+	Width int
+	X, Y, W, H, Ox, Oy int
 }
 
 func New() BitmapFont {
@@ -19,15 +28,23 @@ func New() BitmapFont {
 func (b *BitmapFont) Load(filename string) map[string][]string {
 	file, _ := os.Open(filename)
 	image, _, _ := image.Decode(file)
+	fontdescraw, _ := ioutil.ReadFile(strings.Split(filename, ".")[0]+".json")
 
-	characters := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~-+!@#$%^&*()_={}[]|\\:;\"'<,>.?/"
+	var fontdesc []GlyphInfo
+	err := json.Unmarshal(fontdescraw, &fontdesc)
+	if err != nil {
+		panic(err) 
+	}
+
+	characters := " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 	for idx, c := range strings.Split(characters, "") {
-		xoffset := idx * 8
+		xoffset := fontdesc[idx].X
 		glyph := make([]string, 8)
 		binrep := ""
+		fmt.Println(c)
 
 		for y := 0; y < 8; y++ {
-			for x := 0; x < 8; x++ {
+			for x := 0; x < fontdesc[idx].W; x++ {
 				_, _, _, a := image.At(xoffset + x, y).RGBA()
 				a = a >> 8
 				if a == 255 {
